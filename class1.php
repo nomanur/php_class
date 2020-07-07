739700f99476e749c86975ad8fa5b2ecbb590eb7
nomanur <nomanurrahman@gmail.com>
2020-07-07 19:17:18 +0600

    first-commit

---
+++ class1.php
@@ -0,0 +1,19 @@
+<?php
+
+//echo "hello world";
+
+$number_1 = 1;
+$number_2 = 2;
+$number_3 = 3;
+
+//echo $number_2 % 2;
+
+//$arr = [1=>'one', 'adkalfadljf'=>'two', 3=>'three'];
+
+//array_unshift($arr, 'testdafafa');
+
+//print_r($arr);
+
+
+ 
+
